73ce8d24730684c4fdbe71f53785cd53b5e811a8
小林 <cccc12233@yahoo.co.jp>
2020-03-09 00:23:21 +0900

    UIを整備

---
+++ GithubUserSearch/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  GithubUserSearch
+//
+//  Created by kobayashi riku on 2020/03/09.
+//  Copyright © 2020 koba. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
