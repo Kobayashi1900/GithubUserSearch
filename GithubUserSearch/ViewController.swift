f79576cd581ce14b9f552a3fc55a8576d7b8acfa
小林 <cccc12233@yahoo.co.jp>
2020-03-09 00:26:39 +0900

    必要なライブラリのインポート

--- GithubUserSearch/ViewController.swift
+++ GithubUserSearch/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
