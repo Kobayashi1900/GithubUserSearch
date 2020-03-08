c07a979b9720e98f9b57aef98dfd6170cb7040af
小林 <cccc12233@yahoo.co.jp>
2020-03-09 00:36:28 +0900

    WebViewのサイズ

--- GithubUserSearch/NextViewController.swift
+++ GithubUserSearch/NextViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 import WebKit
 
-class NextViewController: UIViewController {
+class NextViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView = WKWebView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //webViewの大きさ
+        webView.frame = CGRect(x: 0,
+                               y: 0,
+                               width: view.frame.size.width,
+                               height: view.frame.size.height)
+        
     }
     
 
